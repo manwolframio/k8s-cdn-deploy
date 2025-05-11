4b587ac7f3fb493a9d230ca5c0b63afa2065b3ca
Nicolas Manso Nuñez <miguel.manso@edu.uah.es>
2025-05-11 21:42:08 +0200

    Se añade el canary

--- updates/create-canary.sh
+++
@@ -1,7 +0,0 @@
-kubectl create deployment backend-canary \
-  --image=ghcr.io/manwolframio/cdn-backend:v2.2 \
-  --replicas=1 \
-  -n jiome
-
-kubectl label deployment backend-canary app=backend --overwrite -n jiome
-
